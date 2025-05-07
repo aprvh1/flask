1b4ccc086cb57297348bf8211633bc026d1a69c9
aprvh1 <aprvh1@gmail.com>
2025-05-07 22:17:35 +0530

    Update blueprints.py

--- src/flask/blueprints.py
+++ src/flask/blueprints.py
@@ -123,6 +123,7 @@ class Blueprint(SansioBlueprint):
         path = os.path.join(self.root_path, resource)
 
         if mode == "rb":
+            print(path)
             return open(path, mode)  # pyright: ignore
 
         return open(path, mode, encoding=encoding)
